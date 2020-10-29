83f09d94e7948fd433e00497effa26c6887f5b93
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-29 16:45:22 -0400

    Implement distanceTo()

--- Point.java
+++ Point.java
@@ -21,4 +21,7 @@ public class Point{
       return y;
     }  
 
+    public double distanceTo(Point other) {
+        return Math.sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+    }
 }
