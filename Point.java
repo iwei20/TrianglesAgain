8dd1455f4d1b200f96cc0d0af7768ea2362bb161
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-10-29 16:46:03 -0400

    Implement equals()

--- Point.java
+++ Point.java
@@ -24,4 +24,8 @@ public class Point{
     public double distanceTo(Point other) {
         return Math.sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
     }
+
+    public boolean equals(Point other) {
+        return x == other.x && y == other.y;
+    }
 }
